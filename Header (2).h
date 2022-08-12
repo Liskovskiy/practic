0e0c36f2cd6c66f7bb29345529a8a94ce2a8fd07
Liskovskiy <111139295+Liskovskiy@users.noreply.github.com>
2022-08-12 20:29:02 +0300

    Add files via upload

---
+++ Header (2).h
@@ -0,0 +1,7 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+double rad_in_cor(double corner);
+void interpreter(double x_A, double x_B, double x_C, double y_A, double y_B, double y_C, double x_center, double y_center, double corner);
+
+#endif
