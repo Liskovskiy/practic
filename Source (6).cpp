567817e2fbeb0ed2f92788de95aecc81c466d1db
Liskovskiy <111139295+Liskovskiy@users.noreply.github.com>
2022-08-16 15:44:04 +0300

    Add files via upload

---
+++ Source (6).cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "Header.h"
+int main(void)
+{
+    char dictionary[] = "string cola cup umbrella"; 
+    char text[] = "English is a West Germanic language of the";
+    recognition(text, dictionary);
+    return 0;
+}
