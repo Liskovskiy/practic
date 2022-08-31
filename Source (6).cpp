6f83d09b4347e1bb146b173b6644f71fd9df7795
Liskovskiy <111139295+Liskovskiy@users.noreply.github.com>
2022-08-31 20:25:53 +0300

    Delete Source (6).cpp

--- Source (6).cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include "Header.h"
-int main(void)
-{
-    char dictionary[] = "string cola cup umbrella"; 
-    char text[] = "English is a West Germanic language of the";
-    recognition(text, dictionary);
-    return 0;
-}
